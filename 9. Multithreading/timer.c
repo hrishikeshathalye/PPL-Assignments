a69d5b2a08316069bbe0258a34da95501b74210c
Hrishikesh Athalye <hathalye7@gmail.com>
2020-05-19 01:29:52 +0530

    clock now uses spinlocks

--- 9. Multithreading/timer.c
+++ 9. Multithreading/timer.c
@@ -2,9 +2,9 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
-pthread_mutex_t lock;
-//seconds is the shared resource between seconds and minutes
-//minutes is the shared resource between minutes and hours
+pthread_spinlock_t lock1, lock2;
+//seconds is the shared resource between seconds and minutes controlled by lock1
+//minutes is the shared resource between minutes and hours controlled by lock2
 //sync between seconds and minute at 00:00:59
 //sync between the three counters at the instance 00:59:59
 typedef struct myclock{
@@ -14,26 +14,32 @@ typedef struct myclock{
 } myclock;
 myclock c1 = {0, 0, 0};
 void* secondCount(void* status){
-	while((*(char*)status) != '\n'){
+	while(1){
 		sleep(1);
-		//pthread_mutex_lock(&lock);	//output console is the shared resource
+		pthread_spin_lock(&lock1);
 		(c1.seconds)++;
-		//pthread_mutex_unlock(&lock);
+		pthread_spin_unlock(&lock1);
 	}
 }
 void* minuteCount(void* status){ 
-	while((*(char*)status) != '\n'){
+	while(1){
 		if(c1.seconds == 60){
-			(c1.minutes)++;
+			pthread_spin_lock(&lock1);
 			c1.seconds %= 60;
+			pthread_spin_unlock(&lock1);
+			pthread_spin_lock(&lock2);
+			(c1.minutes)++;
+			pthread_spin_unlock(&lock2);
 		}
 	}
 }
 void* hourCount(void* status){
-	while((*(char*)status) != '\n'){
-		if(c1.minutes == 2){
+	while(1){
+		if(c1.minutes == 60){
+			pthread_spin_lock(&lock2);
+			c1.minutes %= 60;
+			pthread_spin_unlock(&lock2);
 			(c1.hours)++;
-			c1.minutes %= 2;
 		}
 	}
 }
@@ -44,25 +50,28 @@ void* printClock(){
 	}
 }
 int main(){
-	char status = '\0';
 	int state;
 	pthread_t id1, id2, id3, id4;
-	if (pthread_mutex_init(&lock, NULL) != 0) { 
-        printf("\n mutex init has failed\n"); 
+	if (pthread_spin_init(&lock1, PTHREAD_PROCESS_SHARED) != 0) { 
+        printf("\n Spinlock init has failed\n"); 
         exit(1); 
     	}
-	printf("Starting Threads (Press Enter to terminate)\n");
-	state = pthread_create(&id1, NULL, secondCount, &status);
+    	if (pthread_spin_init(&lock2, PTHREAD_PROCESS_SHARED) != 0) { 
+        printf("\n Spinlock init has failed\n"); 
+        exit(1); 
+    	}
+	printf("Starting Threads (Press Ctrl+C to terminate)\n");
+	state = pthread_create(&id1, NULL, secondCount, NULL);
 	if(state != 0){
 		printf("Could not create thread, exiting.\n");
 		exit(1);
 	}
-	state = pthread_create(&id2, NULL, minuteCount, &status);
+	state = pthread_create(&id2, NULL, minuteCount, NULL);
 	if(state != 0){
 		printf("Could not create thread, exiting.\n");
 		exit(1);
 	}
-	state = pthread_create(&id3, NULL, hourCount, &status);
+	state = pthread_create(&id3, NULL, hourCount, NULL);
 	if(state != 0){
 		printf("Could not create thread, exiting.\n");
 		exit(1);
@@ -72,13 +81,13 @@ int main(){
 		printf("Could not create thread, exiting.\n");
 		exit(1);
 	}
-	scanf("%c", &status);
-	pthread_join(secondCount, NULL);
-	pthread_join(minuteCount, NULL);
-	pthread_join(hourCount, NULL);
-	pthread_join(printClock, NULL);
+	pthread_join(id1, NULL);
+	pthread_join(id2, NULL);
+	pthread_join(id3, NULL);
+	pthread_join(id4, NULL);
 	printf("Out of The Threads\n");
-	pthread_mutex_destroy(&lock); 
+	pthread_spin_destroy(&lock1);
+	pthread_spin_destroy(&lock2);
 	return 0;
 }
 
